b911e6aaa13f0da1ffc198f431c0f0162593263e
Daniel Alves <danielribeiroalves0@gmail.com>
2022-12-05 22:26:40 -0300

    Adding component for comments.

---
+++ frontend/src/components/UserComment.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="user-comment">
+      <h3>{{title}}</h3>
+      {{text}}
+      {{author}}
+      {{date}}
+      {{anime}}
+      <router-link to="/edit"><button>Edit Comment</button></router-link>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'UserComment',
+  props: {
+    title: String,
+    text: String,
+    author: String,
+    date: String,
+    anime: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.user-comment {
+  margin-bottom: 10px;
+  background-color: rgb(157, 158, 161);
+  border-radius: 10px;
+  padding: 10px;
+  height: 100px;
+}
+</style>
