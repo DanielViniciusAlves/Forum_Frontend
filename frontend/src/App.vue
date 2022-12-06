7ee49e78c60aa9e8459b0cd351b4d373b1611d68
Daniel Alves <danielribeiroalves0@gmail.com>
2022-12-05 22:26:02 -0300

    Remaking the App.vue file for a new Home.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,44 +1,13 @@
 <template>
-      <tbody>
-        <tr v-for="user in users" v-bind:key="user.id"> 
-          <th scope="row">{{user.id}}</th>
-          <td>{{user.title}}</td>
-          <td>{{user.text}}</td>
-          <td>{{user.author}}</td>
-          <td>{{user.date}}</td>
-          <td>{{user.anime}}</td>
-        </tr>
-      </tbody>
+  <router-view/>
 </template>
-
-<script>
-  import axios from 'axios';
-
-export default {
-  name: 'App',
-  data() {
-    return {
-      users: null,
-    };
-  },
-  created: function() {
-    axios
-      .get('http://localhost:4000/comments')
-      .then(res => {
-        console.log(res)
-        this.users = res.data;
-      })
-  }
-}
-</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
