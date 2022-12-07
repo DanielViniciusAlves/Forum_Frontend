7f75777b797701dc0073b5f2a28dae337632c99c
Daniel Alves <danielribeiroalves0@gmail.com>
2022-12-06 21:02:23 -0300

    Improving html organization for home file.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link to="/post"><button>Create New Comment</button></router-link>
+  <HeaderElement :activate_home="true"/>
   <div class="home-component" v-for="user in users" v-bind:key="user.id">
       <div>
         <UserComment :title="user.title" :text="user.text" :author="user.author" :date="user.date" :anime="user.anime"/>
@@ -8,32 +8,36 @@
 </template>
 
 <script>
-// @ is an alias to /src
 import UserComment from '@/components/UserComment.vue'
+import HeaderElement from '@/components/HeaderElement.vue'
 import axios from 'axios';
-  export default {
-    name: "HomeView",
-    components: {
-      UserComment
-    },
-    data() {
-      return {
-        users: null,
-      };
-    },
-    created: function() {
-      axios
-        .get('http://localhost:4000/comments')
-        .then(res => {
-          console.log(res.data)
-          this.users = res.data;
-        })
-    }
+
+export default {
+  name: "HomeView",
+  components: {
+    HeaderElement,
+    UserComment
+  },
+  data() {
+    return {
+      users: null,
+    };
+  },
+  created: function() {
+    axios
+      .get('http://localhost:4000/comments')
+      .then(res => {
+        console.log(res.data)
+        this.users = res.data;
+      })
   }
+}
+
 </script>
 
-<style>
+<style scoped>
 .home-component {
   width: 40%;
 }
+
 </style>
