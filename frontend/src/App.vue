a4454238e36a60a426c65fb10bceb11daacbc600
Daniel Alves <danielribeiroalves0@gmail.com>
2022-12-06 21:00:27 -0300

    Simplifying App file.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,11 +3,9 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
-  color: #2c3e50;
+body {
+  background-color: #000;
+  margin: 0px;
+  font-family: sans-serif;
 }
-</style>
+</style>  
