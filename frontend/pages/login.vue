e94e11d5363babad747a12395a6115a96c31bb0b
Daniel Alves <danielribeiroalves0@gmail.com>
2022-12-10 22:27:42 -0300

    Adding new login page.

---
+++ frontend/pages/login.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>   
+        <LoginComponent @post-send="makeReload" />
+    </div>
+</template>
+  
+<script>
+import LoginComponent from '~/components/LoginComponent.vue'
+export default {
+    name: "LoginPage",
+    components: {
+        LoginComponent
+    },
+    data() {
+        return {
+            componentKey: 0,
+        }
+    },
+    methods: {
+        makeReload() {
+        this.componentKey += 1;
+        }
+    }
+}
+</script>
+
+<style>
+.comments-container { 
+flex-direction: column;
+display: flex;
+justify-content: center;
+align-items: center;
+}
+</style>
